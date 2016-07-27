9f129770a636c766f6cc3f28b6f9f89b3fd6b287
Pietro <donapieppo@yahoo.it>
2016-07-27 12:14:09 +0200

    dm_unibo_common

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Seminaria
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths += %W(#{Rails.root}/app/pdfs)
+    config.time_zone = 'Rome'
+    config.i18n.default_locale = :it
+
+    config.action_mailer.default_url_options = { protocol: 'https' }
+    config.dm_unibo_common = ActiveSupport::HashWithIndifferentAccess.new config_for(:dm_unibo_common)
   end
 end
