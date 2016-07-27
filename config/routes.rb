35408c47a5dbef84d7d674a9bbfe51002ad2fd95
Pietro <donapieppo@yahoo.it>
2016-07-27 12:16:08 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,35 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'seminars/archive/(:year)', to: 'seminars#archive', as: 'archive_seminars'
+  get 'totem',                    to: 'home#totem', as: 'totem'
+
+  get 'user/seminars',   controller: 'seminars',   action: 'index', only_current_user: true, as: 'user_seminars'
+  get 'user/cycles',     controller: 'cycles',     action: 'index',                          as: 'user_cycles'
+
+  resources :seminars do
+    get  :choose_type, on: :collection
+    get  :mail_text,   on: :member
+    post :submit_mail_text, on: :member
+    resources :documents
+    resources :repayments
+  end
+
+  resources :serials do
+    resources :seminars
+  end
+
+  resources :cycles do
+    resources :seminars
+  end
+  
+  resources :repayments do
+    post :notify, on: :member
+    get  :choose_fund, on: :member
+    post :fund, on: :member
+  end
+
+  resources :documents do
+    get :download, on: :member
+  end
+
+  root to: 'home#index'
 end
