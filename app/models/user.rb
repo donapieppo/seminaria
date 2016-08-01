e02a481b171b1c1bb808838d1ae4c38595b0c3b2
Pietro <donapieppo@yahoo.it>
2016-08-01 09:37:28 +0200

    obsolete

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,28 +40,5 @@ class User < ApplicationRecord
     ['Prof.', 'Prof.ssa', 'Dott.', 'Dott.ssa']
   end
    
-  # DSA 
-  
-  def self.update_from_anagrafica_unica(id_anagrafica_unica)
-    user = User.where(id: id_anagrafica_unica.to_i).first
-    if ! user
-      result = dsaSearchClient.find_user(id_anagrafica_unica)
-      if result.count == 0
-        raise NoUser
-      else
-        dsa_user = result.users.inject(nil) do |res, u| 
-          res = u if u.id_anagrafica_unica.to_i == id_anagrafica_unica.to_i
-        end or return NoUser
-        user = User.new({id:      dsa_user.id_anagrafica_unica.to_i,
-                         upn:     dsa_user.upn,
-                         name:    dsa_user.name,
-                         surname: dsa_user.sn})
-        user.save!
-        Rails.logger.info("Created user #{user.inspect}")
-      end
-    end
-    user
-  end
-
 end
 
