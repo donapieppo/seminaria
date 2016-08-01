04fa5e603db98a1380f75b0cb31d06f0f5ff66ad
Pietro <donapieppo@yahoo.it>
2016-08-01 09:41:33 +0200

    admins

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,32 @@
+class AdminsController < ApplicationController
+  before_action :user_cesia!
+
+  def new
+    @organization = Organization.find(params[:organization_id])
+    @admin = @organization.admins.new
+  end
+
+  def create
+    @organization = Organization.find(params[:organization_id])
+    @admin = @organization.admins.new(admin_params)
+    if @admin.save
+      redirect_to organization_path(@organization), notice: "Amministratore aggiunto alla struttura #{@organization.name}."
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    admin = Admin.find(params[:id])
+    admin.destroy and flash[:notice] = 'Amministratore cancellato correttamente.'
+    redirect_to root_path
+  end
+
+  private
+
+  def admin_params
+    params[:admin].permit(:user_upn)
+  end
+end
+
+
