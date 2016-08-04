1bbeb2c42edfe9f41d43d7a3ec10628fcdfa4438
Pietro <donapieppo@yahoo.it>
2016-08-04 16:18:04 +0200

    in place  choice we have also no place

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,7 +4,16 @@ class Place < ApplicationRecord
   default_scope -> { order("name ASC") }
 
   def to_s
-    self.name
+    self.name + " (" + self.address + " " + self.city + ")"
   end
+
+  # other if a fake place in order to specify room by a free string in room_description
+  def self.all_with_other
+    self.all.to_a << self.new(id: 0, 
+                              name: 'Altro', 
+                              address: 'da inserire manualmente', 
+                              city: '')
+  end
+
 end
 
