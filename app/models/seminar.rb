42fc23aa8c96b517133ea3b07bba260f8fb2527b
Pietro <donapieppo@yahoo.it>
2016-08-04 16:18:28 +0200

    some refator... temporary?

--- app/models/seminar.rb
+++ app/models/seminar.rb
@@ -3,33 +3,27 @@ class Seminar < ApplicationRecord
   include Calendar
 
   belongs_to :user
-  belongs_to :room
-  belongs_to :cycle
-  belongs_to :serial
+  belongs_to :organization
+  belongs_to :room, required: false
+  belongs_to :cycle, required: false
+  belongs_to :serial, required: false
   has_many   :documents, dependent: :destroy
   has_one    :repayment, dependent: :destroy
   has_and_belongs_to_many :topics
 
   scope :future, -> { where("date > DATE_ADD(NOW(), INTERVAL -2 hour)") }
 
-  before_save :manage_place_choice, :date_in_future
-
   validates_presence_of :title, :date, :speaker_title, :speaker
 
   # place_id == 1 -> 'non definita'
   # place_id == 2 -> 'esterna' -> si puo' mettere descrizione
-  def manage_place_choice
-    self.place_description = nil unless (self.place_id == 2)
-  end
+  # def manage_place_choice
+  #   self.place_description = nil unless (self.place_id == 2)
+  # end
 
   # convenzione che fino ad un'ora fa non e' passato
   def past?
     self.date < Time.now - 1.hour
-  end
-
-  # non so se sono amministratore
-  # FIXME TODO
-  def date_in_future
   end
 
   def place_to_s
@@ -44,7 +38,7 @@ class Seminar < ApplicationRecord
   end
 
   def arguments_string
-    a = self.arguments
+    a = self.topics
     a.empty? and return ""
     if a.first.name == 'interdisciplinare'
       "seminario interdisciplinare"
@@ -78,6 +72,16 @@ class Seminar < ApplicationRecord
     else
       ""
     end
+  end
+
+  # Accrocchio :-) helps in _form.html.erb for seminar.input :place, collection: ...
+  belongs_to :place
+  def place_id=(id)
+    @place_id = id
+  end
+
+  def place_id
+    @place_id || (self.room_id ? self.room.place_id : nil)
   end
 end
 
