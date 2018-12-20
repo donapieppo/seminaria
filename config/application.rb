16839a2f225ce6d24c5901035955a0271de80fe2
Pietro <donapieppo@yahoo.it>
2018-12-20 14:58:34 +0100

    default in rails new

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,18 @@ Bundler.require(*Rails.groups)
 
 module Seminaria
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
     config.autoload_paths += %W(#{Rails.root}/app/pdfs)
     config.time_zone = 'Rome'
     config.i18n.default_locale = :it
-
-    config.action_mailer.default_url_options = { protocol: 'https' }
+          
+    config.action_mailer.default_url_options = {protocol: 'https'}
     config.dm_unibo_common = ActiveSupport::HashWithIndifferentAccess.new config_for(:dm_unibo_common)
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
   end
 end
