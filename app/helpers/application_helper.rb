6dc239ed0972f84586bfcd93d045afd3b54cc66e
Pietro <donapieppo@yahoo.it>
2019-02-05 13:48:39 +0100

    active storage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,6 +10,7 @@ module ApplicationHelper
   end
 
   def link_to_cv(doc)
-    link_to_download(doc.url, doc.description) 
+    link_to_download(url_for(doc.attach), doc.description) 
+    #link_to_download(rails_blob_url(doc.attach, disposition: "attachment"), doc.description) 
   end
 end
