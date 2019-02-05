181f29eec5be04fbc6f91fa9c3318e8413e11b54
Pietro <donapieppo@yahoo.it>
2019-02-05 13:44:32 +0100

    delete

--- app/inputs/fake_select.rb
+++
@@ -1,7 +0,0 @@
-class FakeSelect < SimpleForm::Inputs::CollectionSelectInput
-  # This method only create a basic input without reading any value from object
-  def select(wrapper_options = nil)
-    merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
-    template.select_field_tag(attribute_name, nil, merged_input_options)
-  end
-end
