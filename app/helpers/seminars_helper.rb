01192eeea899e8949c03241989e40a65199fdd06
Pietro <donapieppo@yahoo.it>
2019-02-05 13:48:10 +0100

    refactor

--- app/helpers/seminars_helper.rb
+++ app/helpers/seminars_helper.rb
@@ -7,21 +7,32 @@ module SeminarsHelper
     res
   end
 
-  def when_tag(seminar, past: false, short: false)
-    date = I18n.l(seminar.date, format: (seminar.past? ? :past : :day))
-    hour = I18n.l(seminar.date, format: :hour)
+  def day_tag(seminar, short: false)
+    wday  = I18n.l(seminar.date, format: :wday)
+    month = I18n.l(seminar.date, format: :month)
+    nday  = I18n.l(seminar.date, format: :nday)
 
     content_tag :div, class: 'date' do
-      icon('calendar').html_safe + '<br/>'.html_safe
-      date.html_safe + ((past or short) ? '' : "<br/>ore #{hour}".html_safe)
+      "<i class='far fa-calendar my-2' style='font-size: 24px'></i><br/>".html_safe + 
+      "<span style='font-weight: 600; font-size: 24px; line-height: 20px;'>#{h nday}<br/>#{h month}</span><br/>".html_safe +
+      (seminar.past? ? "" : "<span class='text-muted'>#{h wday}</span> <br/>".html_safe)
     end
   end
 
-  def where_tag(seminar, past: false, short: false)
+  def hour_tag(seminar, short: false)
+    if seminar.date >= Date.today
+      content_tag :div, class: :hour do
+        icon('clock') + "&nbsp;".html_safe +
+        I18n.t(:hours) + " " + I18n.l(seminar.date, format: :hour)
+      end
+    end
+  end
+
+  def where_tag(seminar, short: false)
     # lascerei il posto fino a ieri (prima era if ! seminar.past?)
-    if seminar.date > Date.today
+    if seminar.date >= Date.today
       content_tag :div, class: :where do
-        #icon('map-marker') +
+        icon('map-marker-alt') + "&nbsp;".html_safe +
         I18n.t(:place) + " " + seminar.place_to_s 
       end
     end
@@ -30,9 +41,9 @@ module SeminarsHelper
   def clock_tag(seminar, short: false)
     if seminar.date.today? and !short
       content_tag :div, class: :today do 
-        concat(big_icon('clock', prefix: 'far'))
+        concat(big_icon('clock', prefix: 'far')) 
         concat( 
-          content_tag(:span) do
+          content_tag(:span, class: 'ml-2') do
             ((seminar.date < Time.now) ? 'iniziato da ' :  'tra ') + time_ago_in_words(seminar.date) 
           end
         )
@@ -40,15 +51,37 @@ module SeminarsHelper
     end 
   end 
 
-  def arguments_tag(seminar)
-    content_tag :div, seminar.arguments_string 
-  end
 
   def abstract_tag(seminar)
     if ! seminar.abstract.blank?  
       content_tag :div, seminar.abstract, class: 'abstract'
     end
   end
+
+  def actions_tag(seminar)
+    capture do 
+      concat(content_tag(:span, icon(:bars, size: 26), class: "actions-button #{policy(seminar).update? ? 'can_update' : ''}"))
+
+      concat(content_tag(:div, class: 'actions-popup') do 
+        if ! seminar.past? 
+          if policy(seminar).update?
+            concat( link_to(fwicon('edit') + ' modifica <br/>'.html_safe, edit_seminar_path(seminar) ))
+          end
+          if user_is_holder?(seminar)
+            repayment_class = (seminar.repayment.fund ? 'fund_ok' : 'fund_missing') 
+            concat( link_to(fwicon('euro-sign') + ' scelta del fondo<br/>'.html_safe, choose_fund_repayment_path(seminar.repayment)) )
+          end 
+          concat ( link_to(fwicon('google', prefix: 'fab') + 'aggiungi a Google Calendar<br/>'.html_safe, seminar.google_url, target: :new) )
+          concat ( link_to(fwicon('calendar-plus') + 'aggiungi a iCal<br/>'.html_safe, seminar_url(seminar, format: :ics)) )
+        end 
+        concat( link_to(fwicon('print') + ' pagina stampabile<br/>'.html_safe, seminar_path(seminar)) )
+        if policy(seminar).destroy?
+          concat( '<br/>'.html_safe + link_to_delete(seminar_path(seminar)) + ' elimina <br/>'.html_safe )
+        end 
+      end)
+    end
+  end
+
 
   def repayment_days_warning
     "La richiesta di compenso / rimborso spese deve essere conclusa con <strong>l'autorizzazione del titolare del fondo</strong> almeno 
