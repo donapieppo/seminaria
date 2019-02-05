d25a24c4896771c4a7973220d20d0bf9683a0af7
Pietro <donapieppo@yahoo.it>
2019-02-05 13:49:10 +0100

    active storage

--- app/models/document.rb
+++ app/models/document.rb
@@ -5,17 +5,15 @@ class Document < ActiveRecord::Base
   belongs_to :repayment, optional: true
   validates :attach_file_name, uniqueness: { scope: :user_id, message: "File già presente nel database." }
 
-  has_attached_file :attach
-
-  validates_attachment_presence :attach
-  do_not_validate_attachment_file_type :attach
+  has_one_attached :attach
 
   def to_s
     self.description.blank? ? self.attach_file_name : self.description
   end
 
   def url
-    "/seminari" + self.attach.url
+    # rails_blob_path(attach, disposition: "attachment")
+    # "/seminari" + self.attach
   end
 end
 
