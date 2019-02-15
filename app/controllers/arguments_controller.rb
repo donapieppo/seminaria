9bb984cf3990b4a54a845c84675f3a58529a0427
Donapieppo <donapieppo@yahoo.it>
2019-02-15 14:01:42 +0100

    arguments

---
+++ app/controllers/arguments_controller.rb
@@ -0,0 +1,41 @@
+class ArgumentsController < ApplicationController
+  before_action :get_argument_and_check_permission, only: [:edit, :update, :destroy]
+
+  def index
+    @arguments = current_organization.arguments
+    authorize Argument
+  end
+
+  def new
+    @argument = current_organization.arguments.new
+    authorize @argument
+  end
+
+  def create
+    @argument = current_organization.arguments.new(name: params[:argument][:name])
+    authorize @argument
+    if @argument.save
+      redirect_to arguments_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @argument.update_attributes(name: params[:argument][:name])
+      redirect_to arguments_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def get_argument_and_check_permission
+    @argument = Argument.find(params[:id])
+    authorize @argument
+  end
+end
