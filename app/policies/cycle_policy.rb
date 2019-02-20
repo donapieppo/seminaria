e448ba9376bdea37783b44feede19d15331de650
Donapieppo <donapieppo@yahoo.it>
2019-02-20 16:30:53 +0100

    cycle_policy

---
+++ app/policies/cycle_policy.rb
@@ -0,0 +1,38 @@
+class CyclePolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    true 
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    @user and (@user.can_manage?(@record.organization_id) or (record.seminar_id and record.seminar.user_id == @user.id))
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    @user and (@user.can_manage?(@record.organization_id) or record.user_id == @user.id)
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    update?
+  end
+
+end
+
