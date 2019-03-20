4239661f554445e8da56ecfb9a0b92ce4a4d0360
Donapieppo <donapieppo@yahoo.it>
2019-03-20 12:36:17 +0100

    Inflector for curriculum_vitae curricula_vitae

--- app/models/curriculum_vitae.rb
+++ app/models/curriculum_vitae.rb
@@ -1,3 +1,7 @@
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+    inflect.irregular 'curriculum_vitae', 'curricula_vitae'
+end
+
 class CurriculumVitae < Document
   belongs_to :repayment, optional: true
 end
