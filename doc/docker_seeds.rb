55d36622f5cd64c0d4c02646d082cfa971e06740
Pietro <donapieppo@yahoo.it>
2019-07-23 15:28:00 +0200

    seeds

--- doc/docker_seeds.rb
+++ doc/docker_seeds.rb
@@ -6,8 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Organization.create(name: "Dip Math Universal", description: "Best of the universe")
+Organization.create(name: "Dip Math", description: "Dipartimento di Matematica di test")
+Organization.create(name: "Dip Chimica", description: "Dipartimento di Chimica di test")
 User.create(id: 1, upn: "user.example@example.com", name: "Nome", surname: "Cognome", email: "user.example@example.com") 
+User.create(id: 2, upn: "admin.name@example.com", name: "MyName", surname: "AdminUser", email: "admin.name@example.com") 
+Argument.create(id: 1, organization_id: 1, name: 'Algebra')
+Argument.create(id: 2, organization_id: 1, name: 'Geometria')
 
 
 
