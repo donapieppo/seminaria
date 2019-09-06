cbc8bffc4ff58dd86bdcef180e7240968ac6b333
Donapieppo <donapieppo@yahoo.it>
2019-09-06 10:31:03 +0200

    authorize

--- app/controllers/funds_controller.rb
+++ app/controllers/funds_controller.rb
@@ -1,20 +1,28 @@
 class FundsController < ApplicationController
   before_action :user_is_admin!
-  before_action :set_fund, only: [:edit, :update, :show, :destroy]
+  before_action :set_fund_and_check_permission, only: [:edit, :update, :show, :destroy]
 
   def index
+    authorize current_organization, :manage?
     @all = params[:all]
     @funds = current_organization.funds.includes(:holder, :category).order('users.surname, users.name, funds.name desc')
     @funds = @funds.where(available: true) unless @all
     @holders = @funds.inject({}) {|res, fund| res[fund.holder] ||= []; res[fund.holder] << fund; res}
   end
 
+  def show
+    @repayments = @fund.repayments.includes(:holder)
+    render layout: false if modal_page
+  end
+
   def new
     @fund = current_organization.funds.new(available: 1)
+    authorize @fund
   end
 
   def create
     @fund = current_organization.funds.new(fund_params)
+    authorize @fund
     if @fund.save
       redirect_to funds_path, notice: "Il fondo è stato creato correttamente."
     else
@@ -31,11 +39,6 @@ class FundsController < ApplicationController
     else
       render action: 'edit'
     end
-  end
-
-  def show
-    @repayments = @fund.repayments.includes(:holder)
-    render layout: false if modal_page
   end
 
   def destroy
@@ -63,8 +66,9 @@ class FundsController < ApplicationController
 
   private
 
-  def set_fund
+  def set_fund_and_check_permission
     @fund = Fund.find(params[:id])
+    authorize @fund
   end
 
   def fund_params
