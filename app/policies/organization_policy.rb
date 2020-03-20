bea92ea779755dd64571e8733018fa55e04e9c7b
Donapieppo <donapieppo@yahoo.it>
2020-03-20 16:10:55 +0100

    moved in dm_unibo_common

--- app/policies/organization_policy.rb
+++
@@ -1,9 +0,0 @@
-class OrganizationPolicy < ApplicationPolicy
-  def read?
-    @user and @user.authorization.can_read?(@record)
-  end
-
-  def manage?
-    @user and @user.authorization.can_manage?(@record)
-  end
-end
