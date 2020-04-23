0b0df3505d25293810e4b720d5d38961f89e9c35
Donapieppo <donapieppo@yahoo.it>
2020-04-23 15:02:59 +0200

    current_organization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,10 +8,5 @@ module ApplicationHelper
   def link_to_id_card(doc)
     link_to_download(url_for(doc.attach), doc.description)
   end
-
-  def current_organization
-    @current_organization
-  end
-
 end
 
--- app/helpers/user_permission_helper.rb
+++ app/helpers/user_permission_helper.rb
@@ -1,9 +1,5 @@
 module UserPermissionHelper
 
-  def current_organization
-    @current_organization
-  end
-
   # see app/policies/application_policy.rb
   def user_is_manager?
     current_user and current_user.authorization.can_manage?(current_organization)
@@ -35,5 +31,4 @@ module UserPermissionHelper
   def redirect_home_with_error
     redirect_to root_path, alert: "Non avete diritti sufficienti per accedere alla pagina richiesta."
   end
-
 end
