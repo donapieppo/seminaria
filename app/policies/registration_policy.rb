2203dbca2af20194d6e210ad2f48693788db120a
Donapieppo <donapieppo@yahoo.it>
2020-05-22 14:42:07 +0200

    policy

--- app/policies/registration_policy.rb
+++ app/policies/registration_policy.rb
@@ -1,19 +1,12 @@
 class RegistrationPolicy < ApplicationPolicy
 
-  def show?
-    @user.id == @record.user_id
-  end
+  # policy @seminar
+  # def index?
+  # end
 
   def create?
     true
   end
 
-  def new_confirmation?
-    true
-  end
-
-  def confirmation?
-    true
-  end
 end
 
