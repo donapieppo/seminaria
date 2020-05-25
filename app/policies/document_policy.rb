0d1d9e548e5c7258fa4c5aec1f292ff260528425
Donapieppo <donapieppo@yahoo.it>
2020-05-25 12:12:05 +0200

    policy

--- app/policies/document_policy.rb
+++ app/policies/document_policy.rb
@@ -2,4 +2,8 @@ class DocumentPolicy < ApplicationPolicy
   def create?
     @user and SeminarPolicy.new(@user, @record.seminar || @record.repayment.seminar).update?
   end
+
+  def destroy?
+    create?
+  end
 end
