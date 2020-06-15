2c2cfa65539abe601422e193a3c459e3f1b9311d
Donapieppo <donapieppo@yahoo.it>
2020-06-15 10:02:27 +0200

    email uniqueness

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -2,6 +2,8 @@ class Registration < ApplicationRecord
   belongs_to :seminar
   belongs_to :user, optional: true
 
+  validates :email, uniqueness: { message: 'Registrazione già effettuata con questa email.' }
+
   def self.session_name(seminar)
     "reg_#{seminar.id}"
   end
