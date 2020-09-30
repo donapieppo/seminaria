adf04acc3b29c07b73392ad217b1207c79c51a91
Donapieppo <donapieppo@yahoo.it>
2020-09-30 16:53:47 +0200

    clean

--- app/controllers/curricula_vitae_controller.rb
+++ app/controllers/curricula_vitae_controller.rb
@@ -1,16 +1,11 @@
 class CurriculaVitaeController < ApplicationController
-  before_action :get_repayment_and_seminar_and_check_permission
 
   def destroy
-   cv = @repayment.curricula_vitae.find(params[:id])
-   authorize cv
-   cv.destroy
-   redirect_to @repayment
-  end
-
-  private
-
-  def get_repayment_and_seminar_and_check_permission
     @repayment = Repayment.find(params[:repayment_id])
+    cv = @repayment.curricula_vitae.find(params[:id])
+    authorize cv
+    cv.destroy
+    redirect_to @repayment
   end
+
 end
