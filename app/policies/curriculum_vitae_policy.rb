8d9b499938631810e0a3aa65da7c26e0f9fae0bc
Donapieppo <donapieppo@yahoo.it>
2020-10-01 11:16:51 +0200

    typo

--- app/policies/curriculum_vitae_policy.rb
+++ app/policies/curriculum_vitae_policy.rb
@@ -1,6 +1,6 @@
 class CurriculumVitaePolicy < DocumentPolicy
   def destroy?
-    @user and RepaymentPolicy.new(@user, @record.repayment).update?
+    @user && RepaymentPolicy.new(@user, @record.repayment).update?
   end
 end
 
