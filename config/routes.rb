a1914ce9ee7c1d2965c93bb621d30f10110dc295
Donapieppo <donapieppo@yahoo.it>
2020-10-16 10:51:09 +0200

    clean old urls

--- config/routes.rb
+++ config/routes.rb
@@ -74,15 +74,4 @@ Rails.application.routes.draw do
     get '/', to: 'seminars#index', as: 'current_organization_root'
   end
   root to: 'seminars#index'
-
-  # OLD delete adter google search is ok
-  get 'serials', to: redirect('mat/serials', status: 302)
-  get 'serials/(:id)', to: redirect('mat/serials/%{id}', status: 302)
-  get 'cycles', to: redirect('mat/cycles', status: 302)
-  get 'cycles/(:id)', to: redirect('mat/cycles/%{id}', status: 302)
-  get 'seminars/archive', to: redirect('mat/seminars/archive', status: 302)
-  get 'seminars/archive/(:year)', to: redirect('mat/seminars/archive/%{year}', status: 302)
-  get 'seminars', to: redirect('mat/seminars', status: 302)
-  get 'seminars/(:id)', to: redirect('mat/seminars/%{id}', status: 302)
-
 end
