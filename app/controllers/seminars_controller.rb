4dd2e636519826faafe693eabcdf063e97713d2e
Donapieppo <donapieppo@yahoo.it>
2021-11-17 10:46:38 +0100

    speaker_on_line

--- app/controllers/seminars_controller.rb
+++ app/controllers/seminars_controller.rb
@@ -172,7 +172,7 @@ class SeminarsController < ApplicationController
       params[:seminar][:date] = params[:seminar][:date] + " " + params[:seminar].delete('date(4i)') + ':' + params[:seminar].delete('date(5i)')
     end
     p = [:date, :duration, :in_presence, :on_line, :meeting_url, :meeting_code, :meeting_visible, 
-         :place_id, :place_description, :speaker_title, :speaker, 
+         :place_id, :place_description, :speaker_title, :speaker, :speaker_on_line, 
          :committee, { argument_ids: [] }, :title, :abstract, :file, :link, :link_text, 
          :serial_id, :cycle_id, :conference_id] # FIXME :serial_id, :cycle_id, :conference_id
     p = p + [:user_id, :serial_id, :cycle_id] if policy(current_organization).manage?
