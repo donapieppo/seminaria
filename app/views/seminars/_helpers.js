5da3206b6e74c0c0971563a826bee683d7850fa0
Pietro <donapieppo@yahoo.it>
2022-01-07 13:36:05 +0100

    no jquery

--- app/views/seminars/_helpers.js
+++ app/views/seminars/_helpers.js
@@ -21,21 +21,19 @@ toggle_div("#seminar_on_line", "#on_line_seminar", null, true);
 toggle_div("#seminar_in_presence", "#in_presence_seminar", null, true);
 toggle_div("#seminar_zoom_meeting_create", "#zoom_auto_div", "#manual_seminar_div", true);
 
-function description_field() {
-  console.log(`description_field`);
-  $("#seminar_place_id option:selected").each(function() {
-    if ($(this).val() == "2") {
-      $(".seminar_place_description").show('slow');
-    } else {
-      $(".seminar_place_description").hide('fast');
-    }
-  });
+function place_description_field() {
+  console.log(`place_description_field`);
+  place_id_selected = document.querySelector('#seminar_place_id').selectedIndex;
+  console.log(place_id_selected);
+  document.querySelector(".seminar_place_description").style.display = (place_id_selected == 1) ? 'block' : 'none';
 }
 
-$(document).ready(function() {
-  description_field();
-  $("#seminar_place_id").change(function() {
-    description_field();
+document.addEventListener("DOMContentLoaded", function(event) {
+  place_description_field();
+  document.querySelectorAll('#seminar_place_id').forEach( (i) => {
+    i.addEventListener('change', () => {
+      place_description_field();
+    });
   });
 });
 
