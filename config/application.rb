5e61e563e92ad29854be4b3aa75bc70892ea06f4
Pietro <donapieppo@yahoo.it>
2022-01-07 12:17:38 +0100

    rails7

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -13,7 +13,7 @@ end
 module Seminaria
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 7.0
     config.hosts << "tester.dm.unibo.it"
     config.hosts << "www.dm.unibo.it"
 
@@ -22,12 +22,15 @@ module Seminaria
     config.i18n.default_locale = :it
 
     config.authlevels = {read: 1, manage: 2}
-          
-    config.action_mailer.default_url_options = {protocol: 'https'}
+
     config.dm_unibo_common = ActiveSupport::HashWithIndifferentAccess.new config_for(:dm_unibo_common)
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
