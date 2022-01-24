da72d76f39d039e8de6423834315246976b3ea82
Pietro <donapieppo@yahoo.it>
2022-01-24 10:08:48 +0100

    old user_is_holder?

--- app/helpers/user_permission_helper.rb
+++ app/helpers/user_permission_helper.rb
@@ -1,22 +1,21 @@
 module UserPermissionHelper
-
   # see app/policies/application_policy.rb
   def user_is_manager?
     current_user && current_organization && current_user.authorization.can_manage?(current_organization)
   end
 
-  def user_is_holder?(what)
-    current_user or return false
-    case what
-    when Repayment
-      what.new_record? and return false
-      what.holder_id == current_user.id
-    when Seminar
-      user_is_holder?(what.repayment)
-    else
-      false
-    end
-  end
+  # def user_is_holder?(what)
+  #   current_user or return false
+  #   case what
+  #   when Repayment
+  #     what.new_record? and return false
+  #     what.holder_id == current_user.id
+  #   when Seminar
+  #     user_is_holder?(what.repayment)
+  #   else
+  #     false
+  #   end
+  # end
 
   # FIXME
   def user_too_late_for_repayment?(seminar)
