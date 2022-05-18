61aa7c2f4418fd9338bc612026398f94e5f8e89d
Pietro <donapieppo@yahoo.it>
2022-05-18 11:02:09 +0200

    where

--- app/javascript/controllers/where_controller.js
+++ app/javascript/controllers/where_controller.js
@@ -1,13 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "place", "online" ]
-  static values = { inpresence: Boolean, online: Boolean }
+  static targets = [ "place", "placedescription", "online", "manual", "zoom" ];
+  static values = { inpresence: Boolean, online: Boolean, zoom: Boolean };
 
   connect() {
-    console.log("in where");
+    console.log("connect where");
     this.placeTarget.style.display = this.inpresenceValue ? 'block' : 'none';
     this.onlineTarget.style.display = this.onlineValue ? 'block' : 'none';
+    this.zoomTarget.style.display = this.zoomValue ? 'block' : 'none';
+    this.manualTarget.style.display = this.zoomValue ? 'none' : 'block';
   }
 
   show_place(el) {
@@ -18,5 +20,15 @@ export default class extends Controller {
   show_online(el) {
     console.log("change online");
     this.onlineTarget.style.display = el.target.checked ? 'block' : 'none';
+  }
+
+  show_zoom(el) {
+    console.log("change zoom");
+    this.zoomTarget.style.display = el.target.checked ? 'block' : 'none';
+    this.manualTarget.style.display = el.target.checked ? 'none' : 'block';
+  }
+
+  show_place_description(el) {
+    console.log("change place");
   }
 }
