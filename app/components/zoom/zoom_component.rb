97ccffed255f16c84b939d1e301a93eea5439b63
Pietro <donapieppo@yahoo.it>
2022-05-18 11:01:35 +0200

    zoom

---
+++ app/components/zoom/zoom_component.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Zoom::ZoomComponent < ViewComponent::Base
+  def initialize(zoom_meeting)
+    @zoom_meeting = seminar.zoom_meeting
+  end
+
+  def render?
+    @zoom_meeting
+  end
+end
+
+
