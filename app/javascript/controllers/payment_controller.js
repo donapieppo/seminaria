e682d73bc2685d5c930cfc7559511222033ad3fa
Pietro <donapieppo@yahoo.it>
2022-05-19 10:11:50 +0200

    REFACTOR

--- app/javascript/controllers/payment_controller.js
+++ app/javascript/controllers/payment_controller.js
@@ -1,68 +1,66 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "paymentBool", "paymentAmount" ];
+  static targets = [ "paymentBool", "paymentDetails", "italyBool", "paymentAmount", "grossBool", "paymentSummary" ];
   static values = {
     irap: String,
-    irpef_italian: String,
-    irpef_foreign: String,
-    adapt_net_italian: String,
-    adapt_net_foreign: String,
-    adapt_gross: String
+    irpefItalian: String,
+    irpefForeign: String,
+    adaptNetItalian: String,
+    adaptNetForeign: String,
+    adaptGross: String
   };
 
   show_lordo_percipiente() {
-    var italy   = document.getElementById("repayment_italy_true").checked;
-    var gross   = document.getElementById("repayment_gross_true").checked; 
-    var payment = document.getElementById("repayment_payment").value || 0;
-    console.log("payment: " + payment);
+    var italy   = this.italyBoolTarget.checked;
+    var gross   = this.grossBoolTarget.checked;
+    var payment = this.paymentAmountTarget.value || 0;
+    console.log(`payment: ${payment} italy: ${italy} gross: ${gross}`);
 
-    gross_result = 0;
-    net_result   = 0;
-    irpef        = italy ? irpef_italian : irpef_foreign; 
-    adapt_net    = italy ? adapt_net_italian_value : adapt_net_foreign_value;
+    var gross_result = 0;
+    var net_result   = 0;
+    var irpef        = italy ? this.irpef_italian : this.irpef_foreign; 
+    var adapt_net    = italy ? this.adapt_net_italian_value : this.adapt_net_foreign_value;
+    console.log(`gross_result: ${gross_result} net_result: ${net_result} irpef: ${irpef} adapt_net: ${adapt_net}`);
 
     if (gross) {
+      net_result   = payment * this.adapt_gross_value * (1 - irpef);  
       gross_result = payment;
-      net_result   = payment * adapt_gross_value * (1 - irpef);  
+      console.log(`gross payment: net_result: ${net_result} gross_result: ${gross_result}`)
     } else {
-      console.log(payment);
-      console.log(adapt_net);
-      console.log(irap);
-      console.log(1.0 + irap);
+      console.log(`correct 1.0 + this.irap: 1.0 + ${this.irap}: ${1.0 + this.irap}`);
       net_result   = payment;
-      gross_result = payment * adapt_net * (1.0 + irap);
-      console.log(gross_result);
+      gross_result = payment * adapt_net * (1.0 + this.irap);
+      console.log(`net payment: net_result: ${net_result} gross_result: ${gross_result}`)
     }
 
-    document.getElementById("payment_summary").innerHTML = "-> <strong>Lordo ente:</strong> " + roundToTwo(gross_result) + " &euro;<br/>" +
-    "-> <strong>Netto:</strong> "      + roundToTwo(net_result)   + " &euro;"; 
+    this.paymentSummaryTarget.innerHTML = "-> <strong>Lordo ente:</strong> " + this.roundToTwo(gross_result) + " &euro;<br/>" +
+                                          "-> <strong>Netto:</strong> " + this.roundToTwo(net_result) + " &euro;"; 
   }
 
   roundToTwo(num) {
     return (+(Math.round(num + "e+2")  + "e-2"));
   }
 
-  highlight_changes(what) {
-    what.classList.add("alert-warning");
+  highlight_changes() {
+    this.paymentSummaryTarget.classList.add("alert-warning");
   }
 
   connect() {
-    const irap                    = parseFloat(this.irapValue);
-    const irpef_italian           = parseFloat(this.irpefItalianValue);
-    const irpef_foreign           = parseFloat(this.irpefForeignValue);
-    const adapt_net_italian_value = parseFloat(this.adaptNetItalianValue);
-    const adapt_net_foreign_value = parseFloat(this.adaptNetForeignValue);
-    const adapt_gross_value       = parseFloat(this.adaptGrossValue);
+    this.irap                    = parseFloat(this.irapValue);
+    this.irpef_italian           = parseFloat(this.irpefItalianValue);
+    this.irpef_foreign           = parseFloat(this.irpefForeignValue);
+    this.adapt_net_italian_value = parseFloat(this.adaptNetItalianValue);
+    this.adapt_net_foreign_value = parseFloat(this.adaptNetForeignValue);
+    this.adapt_gross_value       = parseFloat(this.adaptGrossValue);
+    console.log(`irap: ${this.irap} irpef_italian: ${this.irpef_italian} irpef_foreign: ${this.irpef_foreign} adapt_net_italian_value: ${this.adapt_net_italian_value} adapt_net_foreign_value: ${this.adapt_net_foreign_value} adapt_gross_value: ${this.adapt_gross_value}`)
 
-    display_if_checked(this.paymentAmountTarget, this.paymentBoolTarget);
+    display_if_checked(this.paymentDetailsTarget, this.paymentBoolTarget);
 
     this.show_lordo_percipiente();
-      document.querySelectorAll("#repayment_payment, .repayment_gross, input[name='repayment[italy]']").forEach( (i) => {
-      i.addEventListener('change', (e) => { 
-        highlight_changes(document.getElementById("payment_summary"));
-         show_lordo_percipiente(); 
-      });
+    this.paymentDetailsTarget.addEventListener('change', (e) => { 
+      this.highlight_changes();
+      this.show_lordo_percipiente(); 
     });
   }
 }
