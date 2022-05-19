b81441acee35ad6ac0107d8fece5aba9d2576af2
Pietro <donapieppo@yahoo.it>
2022-05-19 12:45:37 +0200

    old

--- app/javascript/seminaria.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll(".actions-button").forEach( (i) => {
-    i.addEventListener("click", function() {
-      y = i.nextElementSibling;
-      if (y.style.display === "none") { y.style.display = "block"; } else { y.style.display = "none"; }
-    });
-  });
-});
