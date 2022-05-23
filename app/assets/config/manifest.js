9be4e56038dec449dd60045fdf5a85b135b1ce61
Pietro <donapieppo@yahoo.it>
2022-05-23 15:32:45 +0200

    fortawesome

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,2 @@
 //= link_tree ../images
 //= link_tree ../builds
-//= link fa.all.min.css
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
+import * as bootstrap from "bootstrap"
+import "@fortawesome/fontawesome-free/js/all";
 import "@hotwired/turbo-rails"
 import "./controllers";
 import "./seminaria";
