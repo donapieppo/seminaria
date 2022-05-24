c4eed6d1f6d6d94b527ad33bc707d239a6ea98ed
Pietro <donapieppo@yahoo.it>
2022-05-24 11:35:34 +0200

    config

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ end
 
 module Seminaria
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.hosts << "tester.dm.unibo.it"
     config.hosts << "www.dm.unibo.it"
@@ -24,13 +23,5 @@ module Seminaria
     config.authlevels = {read: 1, manage: 2}
 
     config.dm_unibo_common = ActiveSupport::HashWithIndifferentAccess.new config_for(:dm_unibo_common)
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
