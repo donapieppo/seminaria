6657493721a33d110f8d61ddeda1e20c596b777a
Pietro <donapieppo@yahoo.it>
2022-06-07 09:33:05 +0200

    dm_unibo_common

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,3 @@
-import * as bootstrap from "bootstrap"
-import "@fortawesome/fontawesome-free/js/all";
-import "@hotwired/turbo-rails"
-import "./controllers";
+import "dm_unibo_common";
 import "./seminaria";
+import "./controllers";
