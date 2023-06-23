039ed9ec587705425ea79f6f504bc8720ae1775d
Pietro <donapieppo@yahoo.it>
2023-06-23 10:09:35 +0200

    refactor for efficiency

--- app/components/clock_tag_component.rb
+++ app/components/clock_tag_component.rb
@@ -2,11 +2,13 @@
 
 class ClockTagComponent < ViewComponent::Base
   def initialize(date)
-    @date = date
-    @date_string = (@date < Time.now ? ' iniziato da ' :  ' tra ') + time_ago_in_words(@date) 
+    if date.today?
+      @date = date
+      @date_string = ((@date < Time.now) ? " iniziato da " : " tra ") + time_ago_in_words(@date)
+    end
   end
 
   def render?
-    @date.today? 
+    @date
   end
 end
