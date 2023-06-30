c826926e29ea9985909c161dd93bcecdee722459
Pietro <donapieppo@yahoo.it>
2023-06-30 13:16:28 +0200

    new components

---
+++ app/components/hour_tag_component.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class HourTagComponent < ViewComponent::Base
+  def initialize(seminar)
+    @seminar = seminar
+  end
+end
---
+++ app/components/where_tag_component.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class WhereTagComponent < ViewComponent::Base
+  def initialize(seminar)
+    @seminar = seminar
+  end
+end
