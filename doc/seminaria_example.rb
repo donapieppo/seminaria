f7df2608dd60e5c4cf39eda7191fec741143a8d8
Pietro <donapieppo@yahoo.it>
2023-07-18 16:51:29 +0200

    doc

--- doc/seminaria_example.rb
+++ doc/seminaria_example.rb
@@ -1,31 +1,28 @@
-CESIA_UPN = ['administrator@example.com']
+CESIA_UPN = ["pietro.donatini@unibo.it"]
 
 module Seminaria
   class Application < Rails::Application
-    config.domain_name        = 'example.it'
+    config.domain_name = "dm.unibo.it"
 
-    # exammple: with https://example.it/math your session is on Organization.find(1)
-    config.organizations_urls = { 'math'      => 1,
-                                  'chemistry' => 2,
-                                  'disi'      => 3 }
-
-    config.header_icon        = 'microphone'
-    config.header_title       = 'Seminari'
-    config.header_subtitle    = 'Università di Bologna'
+    config.html_title = "Seminari del Dipartimento di Matematica"
+    config.html_description = "Gestione seminari e Notizie del Dipartimento di Matematia. Università di Bologna."
+    config.header_title = "Seminari"
+    config.header_subtitle = "del Dipartimento di Matematica"
+    config.header_icon = "bullhorn"
     config.repayment_deadline = 14 # 8 days for working on repayment/refund before speaker arrival
     config.on_line_repayment_deadline_1 = 5  # 5 giorni prima della giunta
-    config.on_line_repayment_deadline_2 = 15 # 15 giorni prima del seminario
+    config.on_line_repayment_deadline_2 = 10 # 10 fra la giunta e inizio del seminario
 
-    # email from field
-    config.default_from    = 'DipMat Seminari <pippo.pluto@example.com>'
-    config.reply_to        = 'support@example.com'
+    config.default_from = "DipMat Seminari <notifica@example.it>"
+    config.reply_to = "notifica@example.it"
 
     config.dm_unibo_common.update(
-      login_method:        :log_and_create,
-      message_footer:      %Q{Messaggio inviato da 'Gestione Seminari Dipartimento di Matematica'.\nNon rispondere a questo messaggio.\nPer problemi tecnici contattare testmail@example.comd},
-      impersonate_admins:  ['administrator@example.com'],
-      interceptor_mails:   ['name.surname2@examplexample.com'], 
+      login_method: :log_and_create,
+      no_students: true,
+      message_footer: %{Messaggio inviato da 'Gestione Seminari Dipartimento di Matematica'.\nNon rispondere a questo messaggio.\nPer problemi tecnici contattare dipmat-supportoweb@unibo.it},
+      impersonate_admins: ["name@unibo.it", "name2@unibo.it"],
+      interceptor_mails: ["example@unibo.it"],
+      main_impersonations: ["name@unibo.it"]
     )
   end
 end
-
