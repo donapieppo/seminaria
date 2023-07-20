d83645d638b5d8cb3be257d9d61ad9a8a4796863
Pietro <donapieppo@yahoo.it>
2023-07-20 12:34:55 +0200

    rubocop

--- doc/docker_seeds.rb
+++ doc/docker_seeds.rb
@@ -8,11 +8,8 @@
 
 Organization.create(name: "Dip Math", description: "Dipartimento di Matematica di test")
 Organization.create(name: "Dip Chimica", description: "Dipartimento di Chimica di test")
-User.create(id: 1, upn: "administrator@example.com", name: "Name", surname: "Administrator", email: "administrator@example.com") 
-User.create(id: 2, upn: "manager@example.com", name: "Name", surname: "Manager", email: "manager@example.com") 
-User.create(id: 3, upn: "user@example.com", name: "Name", surname: "Surname", email: "user@example.com") 
-Argument.create(organization_id: 1, name: 'Algebra')
-Argument.create(organization_id: 1, name: 'Geometria')
-
-
-
+User.create(id: 1, upn: "administrator@example.com", name: "Name", surname: "Administrator", email: "administrator@example.com")
+User.create(id: 2, upn: "manager@example.com", name: "Name", surname: "Manager", email: "manager@example.com")
+User.create(id: 3, upn: "user@example.com", name: "Name", surname: "Surname", email: "user@example.com")
+Argument.create(organization_id: 1, name: "Algebra")
+Argument.create(organization_id: 1, name: "Geometria")
