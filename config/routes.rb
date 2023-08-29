ae32a29bad524ef8fc10b7c87257cb5be44e3ba5
Pietro <donapieppo@yahoo.it>
2023-08-29 14:54:55 +0200

    page

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   scope ":__org__" do
     get "seminars/archive/(:year)", to: "seminars#archive", as: "archive_seminars"
-    get "seminars/:year/:mm/:dd/:slug", to: "seminars#page", year: /20\d\d/, mm: /\d\d/, dd: /\d\d/, as: :single_page
+    get "seminars/:year/:mm/:dd/:speaker", to: "seminars#page", year: /20\d\d/, mm: /\d\d/, dd: /\d\d/, as: :single_page
 
     get "totem", controller: "home", action: "totem", as: "totem"
 
