519d9aac01ee600a52d17fbc55f45e3aaf140e93
Pietro <donapieppo@yahoo.it>
2024-03-08 10:57:09 +0100

    locale

--- app/helpers/money_helper.rb
+++ app/helpers/money_helper.rb
@@ -1,6 +1,6 @@
 include ActionView::Helpers::NumberHelper
 module MoneyHelper
   def euro(num)
-    num ? number_to_currency(num, unit: "€", separator: ",") : "-"
+    num ? number_to_currency(num, unit: "€", separator: ",", locale: :it) : "-"
   end
 end
