dbfa82e4174d671b7358e92b82efb8199410f63a
Pietro <donapieppo@yahoo.it>
2024-03-20 13:28:40 +0100

    rubocop

--- app/policies/speaker/repayment_policy.rb
+++ app/policies/speaker/repayment_policy.rb
@@ -1,16 +1,17 @@
 class Speaker::RepaymentPolicy < ApplicationPolicy
   # simple user can update until notified
   def update?
-    @user and (@user.authorization.can_manage?(@record.seminar.organization_id) or 
-              ((! @record.notified) and SeminarPolicy.new(@user, @record.seminar).update?))
+    (@user && @user.authorization.can_manage?(@record.seminar.organization_id)) ||
+      (@user && !@record.notified && SeminarPolicy.new(@user, @record.seminar).update?)
   end
 
   def data_request?
-    update? and (! @record.notified)
+    update? && !@record.notified
+    true
   end
 
   def submit_data_request?
-    update? and (! @record.notified)
+    update? && !@record.notified
   end
 
   # skip_before_action :redirect_unsigned_user, only: [:accept, :show, :edit, :update]
