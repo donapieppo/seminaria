3899458945287997a5ab906732c783bb82661fbf
Pietro Donatini <pietro.donatini@unibo.it>
2025-01-14 15:32:52 +0100

    config

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   # default 'Content-Transfer-Encoding' => '7bit'
-  default from: Rails.configuration.default_from
+  default from: Rails.configuration.unibo_common.default_from
   layout "mailer"
 end
--- app/mailers/seminar_mailer.rb
+++ app/mailers/seminar_mailer.rb
@@ -1,12 +1,12 @@
 class SeminarMailer < ApplicationMailer
   def notify_seminar(seminar, to, subject, text)
     @seminar = seminar
-    @to      = to
-    @text    = text
-    mail(to:       to,
-         reply_to: Rails.configuration.reply_to,
-         subject:  subject)
+    @to = to
+    @text = text
+    mail(
+      to: to,
+      reply_to: Rails.configuration.unibo_common.reply_to,
+      subject: subject
+    )
   end
 end
-
-
