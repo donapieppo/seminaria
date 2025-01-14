6bd05a60fee14abe9db4fc771b6db32ad06ee348
Pietro Donatini <pietro.donatini@unibo.it>
2025-01-14 13:31:02 +0100

    update

--- config/application.rb
+++ config/application.rb
@@ -14,21 +14,14 @@ module Seminaria
   class Application < Rails::Application
     config.load_defaults 7.1
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.hosts += ENV.fetch("ALLOWED_HOSTS", "").split(",")
 
-    config.hosts << "tester.dm.unibo.it"
-    config.hosts << "www.dm.unibo.it"
-
-    config.autoload_paths << "#{Rails.root}/app/pdfs"
     config.time_zone = "Rome"
     config.i18n.default_locale = :it
 
     config.authlevels = {read: 1, manage: 2}
 
     config.action_mailer.default_url_options = {protocol: "https"}
-    config.dm_unibo_common = ActiveSupport::HashWithIndifferentAccess.new config_for(:dm_unibo_common)
+    config.unibo_common = config_for(:unibo_common)
   end
 end
