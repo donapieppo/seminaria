6f7428d9026cde140c6c73b092cbfcb1aca362c5
Pietro Donatini <pietro.donatini@unibo.it>
2025-01-17 09:35:25 +0100

    refactor

--- app/components/day_tag_component.rb
+++ app/components/day_tag_component.rb
@@ -2,18 +2,19 @@
 
 class DayTagComponent < ViewComponent::Base
   def initialize(what)
-    if what.is_a?(Seminar)
-      if what.conference
-        @conference = what.conference
-      else
-        @seminar = what
-        @month = I18n.l(@seminar.date, format: :month)
-        @nday = I18n.l(@seminar.date, format: :nday)
-        # week day for future seminar or year if already done
-        @detail = (@seminar.date < Time.now) ? @seminar.date.year : I18n.l(@seminar.date, format: :wday).capitalize
-      end
+    @conference = if what.is_a?(Seminar)
+      what.conference
     else
-      @conference = what
+      what
     end
+
+    @date = if @conference
+      @conference.start_date
+    else
+      what.date
+    end
+
+    # week day for future seminar or year if already done
+    @detail = (@date < Time.now) ? @date.year : I18n.l(@date, format: :wday).capitalize
   end
 end
