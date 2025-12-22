b09590ed3e3a9278e804b4803b95c6b1a8704de1
Pietro Donatini <pietro.donatini@unibo.it>
2025-12-22 09:12:29 +0100

    helper

--- app/components/seminar/seminar_component.rb
+++ app/components/seminar/seminar_component.rb
@@ -1,9 +1,20 @@
 # frozen_string_literal: true
 
 class Seminar::SeminarComponent < ViewComponent::Base
+  include DmUniboCommon::ApplicationHelper
+
   def initialize(seminar, current_user, short: false)
     @seminar = seminar
     @current_user = current_user
     @short = short
+  end
+
+  def css_classes(seminar)
+    res = "seminar"
+    res += seminar.past? ? " past" : " future"
+    res += " seminar-serial" if seminar.serial_id
+    res += " seminar-cycle" if seminar.cycle_id
+    res += " seminar-conference" if seminar.conference_id
+    res
   end
 end
